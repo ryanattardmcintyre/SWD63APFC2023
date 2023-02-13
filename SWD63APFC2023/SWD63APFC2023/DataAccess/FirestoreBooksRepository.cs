24bc01b4c412fc6ed7e5f88a84a468cbc7244449
Ryan Attard <ryanattard@gmail.com>
2023-02-13 11:33:15 +0100

    firestore part 1

---
+++ SWD63APFC2023/SWD63APFC2023/DataAccess/FirestoreBooksRepository.cs
@@ -0,0 +1,38 @@
+﻿using Google.Cloud.Firestore;
+using SWD63APFC2023.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWD63APFC2023.DataAccess
+{
+    public class FirestoreBooksRepository
+    {
+        FirestoreDb db;
+
+        public FirestoreBooksRepository(string project)
+        {
+             db = FirestoreDb.Create(project);
+        }
+
+        public async void AddBook(Book b)
+        {
+                await db.Collection("books").Document().SetAsync(b);
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            List<Book> books = new List<Book>();
+
+            Query allBooksQuery = db.Collection("books");
+            QuerySnapshot allBooksQuerySnapshot = await allBooksQuery.GetSnapshotAsync();
+            foreach (DocumentSnapshot documentSnapshot in allBooksQuerySnapshot.Documents)
+            {
+                Book book = documentSnapshot.ConvertTo<Book>();
+                books.Add(book);
+            }
+            return books;
+        }
+    }
+}
---
+++ SWD63APFC2023/SWD63APFC2023/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SWD63APFC2023.Models
+{
+    [FirestoreData]
+    public class Book
+    {
+        [FirestoreProperty]
+        public string Isbn { get; set; }
+        
+        [FirestoreProperty]
+        public string Name { get; set; }
+        
+        [FirestoreProperty]
+        public string Author { get; set; }
+        
+        [FirestoreProperty]
+        public int Year { get; set; }
+
+    }
+}
